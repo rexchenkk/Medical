131c3b5aa4ca9bfe20011c0ed75cd8450f83c361
Rex <chenhaojie@dayuwuxian.com>
2023-12-08 16:52:12 +0800

    Shift to usual andrid ui

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,17 @@
-rootProject.name = "Medical"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
-    repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
-
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
-include(":composeApp")
+rootProject.name = "Medical"
+include(":app")
